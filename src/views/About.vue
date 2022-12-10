882e40a78883945cc5f85da4b5c345a145b3620e
Buzatu Bianca <95959789+biancabuzatu2000@users.noreply.github.com>
2022-12-10 23:23:52 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,1 +1,71 @@
+<template>
+    <main class="about-page main-grid">
 
+        <h2>WE BAKE TOGETHERNESS</h2>
+        <p>
+            At the heart of each  product and experience is connection.<br>
+            To shop at us is to mark a moment, no matter how big or small, and to share that moment with others. <br>
+            If our products are artful, quality baked goods, then their benefits are enjoyment and sharing. <br>
+            It’s about choosing kindness for yourself and others, in both the good and not so good times. <br>
+
+           Our place inspires togetherness. We exist to help people connect.<br>
+            To celebrate. To console. To remember. To play. <br>
+            For family, old and new. For friends, neighbours, and co-workers. <br>
+            Each batch, each bite, is special. Full of beautiful memories.<br>
+            We are the place our community turns to mark important dates and milestones, or to indulge in a bite of everyday enjoyment.<br>
+            For moments big and small. For joy, warmth and inclusion. <br>
+            With skill, care and kindness, we produce quality baking that’s as scrumptious and special as the people who share it.<br><br>
+        </p>
+    </main>
+</template>
+<style lang="scss">
+
+
+.about-page {
+    background-image: url("https://img5.goodfon.com/wallpaper/nbig/7/a3/pirozhnoe-kapkeik-krem-shokolad-keks.jpg");
+    height: 100%;
+    width: 100%;
+    border: none;
+    padding: 560px 0;
+    background-repeat: no-repeat;
+    background-size: cover;
+    @media only screen and (min-width:0) {
+        h2{
+            color: white;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            font-size: 24px;
+            text-align: center;
+            padding: 50px;
+
+        }
+    }
+
+    @media only screen and (min-width: 1024px) {
+        h2 {
+            color: white;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            font-size: 44px;
+            text-align: center;
+            display: block;
+
+        }
+
+
+    }
+    p {
+
+        font-size: 34px;
+        color:white;
+        text-transform: initial;
+        letter-spacing: 1px;
+        display: block;
+        text-align: center;
+    }
+}
+
+       
+
+
+   </style>
